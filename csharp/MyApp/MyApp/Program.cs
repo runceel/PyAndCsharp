fbb7059083bdad0690561ca2806bcf4cc7831303
Kazuki Ota <kaota@microsoft.com>
2022-01-24 19:12:11 +0900

    csvの読み込みが遅い...

--- csharp/MyApp/MyApp/Program.cs
+++ csharp/MyApp/MyApp/Program.cs
@@ -1,55 +1,49 @@
-﻿using System.Diagnostics;
-using Newtonsoft.Json;
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-namespace MyApp
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var testData = new List<TestData>();
+using (var reader = new StreamReader("test.csv"))
 {
-    class Program
+    reader.ReadLine();
+    while (!reader.EndOfStream)
     {
-        static void Main(string[] args)
-        {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+        var s = reader.ReadLine();
+        var columns = s.Split(',');
 
-            var testData = new List<TestData>();
-            using (var reader = new StreamReader("test.csv"))
-            {
-                reader.ReadLine();
-                while (!reader.EndOfStream)
-                {
-                    string s = reader.ReadLine();
-                    var columns = s.Split(',');
-                    testData.Add(new TestData
-                    {
-                        a = columns[0],
-                        b = columns[1],
-                        x = double.Parse(columns[2]),
-                        y = double.Parse(columns[3])
-                    });
-                }
-            }
-
-            var result = testData
-                .Select(d => (d.a, d.b, d.x, d.y, z: MultiplyToInt(d.x, d.y)))
-                .GroupBy(d => d.a)
-                .Select(g => new ResultData(a: g.Key, sum: g.Sum(d => d.z)));
-            using (StreamWriter file = File.CreateText("result.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, result);
-            }
-
-            var ts = stopwatch.Elapsed;
-            Console.WriteLine($"処理時間: {ts.TotalSeconds}秒");
-        }
-
-        static int MultiplyToInt(double x, double y)
-        {
-            if (x > 0)
-                return (int)(x * y + 0.0000001);
-            return (int)(x * y - 0.0000001);
-        }
+        testData.Add(new TestData(
+            columns[0],
+            columns[1],
+            double.Parse(columns[2]),
+            double.Parse(columns[3])));
     }
-
-    record struct TestData(string a, string b, double x, double y);
-    record struct ResultData(string a, int sum);
 }
+Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+
+var result = testData
+    .GroupBy(d => d.a)
+    .Select(g => new ResultData(a: g.Key, sum: g.Sum(d => MultiplyToInt(d.x, d.y))));
+using (FileStream file = File.OpenWrite("result.json"))
+{
+    JsonSerializer.Serialize(file, result, SourceGenerationContext.Default.IEnumerableResultData);
+}
+
+var ts = stopwatch.Elapsed;
+Console.WriteLine($"処理時間: {ts.TotalSeconds}秒");
+
+int MultiplyToInt(double x, double y)
+{
+    if (x > 0)
+        return (int)(x * y + 0.0000001);
+    return (int)(x * y - 0.0000001);
+}
+
+[JsonSerializable(typeof(IEnumerable<ResultData>))]
+internal partial class SourceGenerationContext : JsonSerializerContext { }
+
+record struct TestData(string a, string b, double x, double y);
+record struct ResultData(string a, int sum);
